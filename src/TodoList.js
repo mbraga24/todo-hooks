df930a297b9672e81b2d4e4f16a161f61dbb6a45
github username <rdg.brg@gmail.com>
2020-08-06 18:25:46 -0400

    Import material ui components / set up and display todos

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import Paper from '@material-ui/core/Paper';
+import Divider from '@material-ui/core/Divider';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 
 const TodoList = (props) => {
   return (
-    <div>
-      {props.todos.map(todo => (
-        <li>{todo.task}</li>
-      ))}
-    </div>
+    <Paper>
+      <List>  
+        {props.todos.map(todo => (
+        <>
+          <ListItem>
+            <ListItemText>{todo.task}</ListItemText>
+          </ListItem>
+          <Divider/>
+        </>
+        ))}
+      </List>
+    </Paper>
   )
 }
 
